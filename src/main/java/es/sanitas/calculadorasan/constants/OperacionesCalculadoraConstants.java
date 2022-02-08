6249844d89ac490ea93720dad40c2f2958195d24
Julio <X482295@santander.com>
2022-02-08 18:08:42 +0100

    Añadido modelo y clase constantes

---
+++ src/main/java/es/sanitas/calculadorasan/constants/OperacionesCalculadoraConstants.java
@@ -0,0 +1,17 @@
+package es.sanitas.calculadorasan.constants;
+
+/**
+ * Clase de constantes
+ * 
+ * @author Julio
+ *
+ */
+public class OperacionesCalculadoraConstants {
+
+	private OperacionesCalculadoraConstants(){}
+
+	public static final String SUMAR = "suma";
+	public static final String RESTAR = "resta";
+	public static final String MULTIPLICAR = "multiplicacion";
+	public static final String DIVIDIR = "division";
+}
