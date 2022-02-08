8525fec6c6982bf3dada6b9cb325e66999ebbb53
Julio <X482295@santander.com>
2022-02-08 18:06:49 +0100

    Primer commit

---
+++ src/main/java/es/sanitas/calculadorasan/CalculadoraSanApplication.java
@@ -0,0 +1,13 @@
+package es.sanitas.calculadorasan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadoraSanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculadoraSanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/sanitas/calculadorasan/CalculadoraSanApplicationTests.java
@@ -0,0 +1,13 @@
+package es.sanitas.calculadorasan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadoraSanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
