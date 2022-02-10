209a10bc0c00869e932323aafde8371134901abc
Julio <X482295@santander.com>
2022-02-10 15:38:56 +0100

    Añadido Sleuth y Swagger

--- src/main/java/es/sanitas/calculadorasan/controller/OperacionesController.java
+++ src/main/java/es/sanitas/calculadorasan/controller/OperacionesController.java
@@ -1,6 +1,15 @@
 package es.sanitas.calculadorasan.controller;
 
 import io.corp.calculator.TracerImpl;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -12,6 +21,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import es.sanitas.calculadorasan.services.OperacionesCalculadoraService;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
 
 /**
  * Clase que se encargara de exponer los endpoints de operaciones de la calculadora
@@ -37,11 +48,33 @@ public class OperacionesController {
 	 *
 	 * @return ResponseEntity con la respuesta
 	 */
+	@Operation(description = "math operator for calculator application")
+	// Body swagger
+	@Parameters(value = {
+			@Parameter(name = "numero1", in= ParameterIn.QUERY , required = true,
+					description = "first number to calculate"),
+			@Parameter(name = "numero2", in= ParameterIn.QUERY , required = true,
+					description = "second number to calculate"),
+			@Parameter(name = "operacion", in= ParameterIn.QUERY , required = true,
+					description = "operation action")})
+	// Códigos de respuesta swagger
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "204", description = "No Content"),
+			@ApiResponse(responseCode = "400", description = "Bad Request", content =
+			@Content(schema = @Schema(implementation = HttpClientErrorException.class))),
+			@ApiResponse(responseCode = "401", description = "Unauthorized", content =
+			@Content(schema = @Schema(implementation = HttpClientErrorException.class))),
+			@ApiResponse(responseCode = "403", description = "Forbidden", content =
+			@Content(schema = @Schema(implementation = HttpClientErrorException.class))),
+			@ApiResponse(responseCode = "404", description = "Not Found", content =
+			@Content(schema = @Schema(implementation = HttpClientErrorException.class))),
+			@ApiResponse(responseCode = "500", description = "Internal server error", content =
+			@Content(schema = @Schema(implementation = HttpServerErrorException.class))) })
 	@GetMapping("/operar")
 	public ResponseEntity<String> operarNumerosEndpoint(
-			@RequestParam Double numero1,
-			@RequestParam Double numero2,
-			@RequestParam String operacion) {
+			@RequestParam("numero1") Double numero1,
+			@RequestParam("numero2") Double numero2,
+			@RequestParam("operacion") String operacion) {
 		Double resultado = 0.0;
 		TracerImpl tracer = new TracerImpl();
 		try{
